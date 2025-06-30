cd8968b0a1d3e1c07c09e282ca2c909ff2ae9df3
krishna-warfade <krishnawarfade@gmail.com>
2025-06-30 22:24:35 +0530

    Add delete functionality to BlogList component and update HomePage to handle blog deletions

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,10 +7,14 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id); //to store the blogs in array except the one with the id that is passed
+        setBlogs(newBlogs);
+    };
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
-            <BlogList blogs={blogs.filter((blog) =>blog.author==="mario")} title="Mario's Blogs"/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs.filter((blog) =>blog.author==="mario")} title="Mario's Blogs"/> */}
             </div>
      );
 }
