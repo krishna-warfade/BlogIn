83b380c26a8a6cea12cb70603056d83efc300c59
krishna-warfade <krishnawarfade@gmail.com>
2025-07-01 17:21:14 +0530

    updated

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,11 +8,17 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs') //fetching data from db.json
             .then(res => {
+                if(!res.ok) {
+                    throw Error('Could not fetch the data for that resource'); //thrwg error if response is not ok
+                }
                return res.json(); //converting response to json
             })
             .then(data => {
                 setBlogs(data); //setting blogs state with the fetched data
                 setIsPending(false); //setting isPending to false after data is fetched
+            })
+            .catch(err => {
+                console.log(err.message); //logging error message if any error occurs during fetching
             })
         }, 1000); //Simulating a delay of 1 second before fetching data
     }, []);
