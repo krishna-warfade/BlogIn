412f2bab157933dc053a528c30dbb68c1a412825
krishna-warfade <krishnawarfade@gmail.com>
2025-07-03 11:07:18 +0530

    Add Create component with form for new blog entries; update styles for consistency

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,35 @@
+import {useState} from 'react';
 const Create = () => {
+    const [title, setTitle] = useState(''); // State to hold the blog title
+    const [body, setBody] = useState(''); // State to hold the blog body
+    const [author, setAuthor] = useState('mario'); // State to hold the blog author, defaulting to 'mario'
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <input 
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                />
+                <label>Blog author:</label>
+                <select>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
      );
 }
