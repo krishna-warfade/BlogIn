9038af2f34e179856fddb442c0d4676145e577ce
krishna-warfade <krishnawarfade@gmail.com>
2025-07-03 10:33:15 +0530

    Enhance BlogDetails component with loading and error handling; update styles for blog details page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch"; // Custom hook to fetch data from the API
 
 const BlogDetails = () => {
     const { id } = useParams(); // useParams hook is used to access the dynamic parameters from the URL, such as blog ID
+    const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id); // Fetching blog details using the blog ID from the URL
     return ( 
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            {isPending && <div className="loader">Loading...</div>} {/* Display loading message while fetching data */}
+            {error && <div>{error}</div>} {/* Display error message if there is an error fetching data */}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2> {/* Display blog title */}
+                    <p>Written by {blog.author}</p> {/* Display blog author */}
+                    <div>{blog.body}</div> {/* Display blog content */}
+                </article>
+            )}
         </div>
      );
 }
